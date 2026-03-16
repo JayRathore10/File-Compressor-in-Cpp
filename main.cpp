bd9376fd85aa87082e667863ce7f1f0b6788c7ce
Jay <jayrathore88155@gmail.com>
2026-03-16 19:43:15 +0530

    Add sample.txt and also add a function to find its size

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
+#include<fstream>
+
+#define ll long long 
 
 using namespace std ;
 
+double convertByteToMB(ll);
+
 int main(int argc , char** argv){
   cout<<"File compressor"<<endl;
-  return 0;
+  ifstream file("sample.txt");
+
+  if(!file){
+    cout<<"Error in file opening"<<endl;
+    return 0;
+  }
+
+  file.seekg(0 , ios::end);
+  ll size = file.tellg();
+
+  cout<<"File size : "<<(convertByteToMB(size))<<endl;
+
+  file.close();
+  return 0; 
+}
+
+double convertByteToMB(ll bytes){
+  return ((double)bytes / 1000000);
 }
